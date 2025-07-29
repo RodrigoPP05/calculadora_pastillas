d573414e1bfb2cfa3fbb8c5f7f0170489ed89b15
RodrigoPP05 <rodrigo.ponce@galileo.edu>
2025-07-28 20:44:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,6 @@ def index():
     return render_template("index.html", resultado=resultado, plot_data=json.dumps(plot_data))
 
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=10000)
